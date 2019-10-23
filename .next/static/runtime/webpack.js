4a24b3a160d959d5815130adeebb31c90a356235
Abby Gottlich <abby.gottlich@gmail.com>
2019-10-23 10:03:27 -0500

    index.js page created and rendering properly

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "3b5b7646d2970709e6ad";
+/******/ 	var hotCurrentHash = "620c7a3a9b1e85744467";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
